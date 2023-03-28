6bb13fb747383f822b99d2d4ae76b79dd5371e95
max <109116116+maxlcas@users.noreply.github.com>
2023-03-28 09:54:22 +0100

    dereference offset to get index

--- array_reader/reader/file_reader.v
+++ array_reader/reader/file_reader.v
@@ -4,9 +4,7 @@ pub fn read_int(bytes []u8, mut offset &int) int {
 	defer {
 		offset += 4
 	}
-
-	offset_d := offset // forced by compiler bug, to be fixed. (https://github.com/vlang/v/issues/17800)
-	return bytes[offset_d] | ( int(bytes[offset_d + 1]) << 8 ) | ( int(bytes[offset_d + 2]) << 16 ) | ( int(bytes[offset_d + 3]) << 24 )
+	return bytes[*offset] | ( int(bytes[*offset + 1]) << 8 ) | ( int(bytes[*offset + 2]) << 16 ) | ( int(bytes[*offset + 3]) << 24 )
 }
 
 pub fn read_f32(bytes []u8, mut offset &int) f32 { // not complete
